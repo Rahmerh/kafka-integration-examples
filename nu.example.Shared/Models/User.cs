8cde6aa9af1ec7c07a9d2a0e1ad1f27adb412b87
Bas Bouwhuis <bas@Bass-MacBook-Pro.local>
2022-09-23 14:02:20 +0200

    First setup of the project & early consumer

---
+++ nu.example.Shared/Models/User.cs
@@ -0,0 +1,8 @@
+namespace nu.medapp.Shared.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public String LastName { get; set; }
+}
---
+++ nu.example.UserConsumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "host.docker.internal:9094",
+            SaslMechanism = SaslMechanism.Plain,
+            GroupId = Guid.NewGuid().ToString(),
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var c = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            c.Subscribe("");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = c.Consume(cts.Token);
+                        Console.WriteLine($"Consumed message '{cr.Message.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Close and Release all the resources held by this consumer  
+                c.Close();
+            }
+        }
+    }
+}
